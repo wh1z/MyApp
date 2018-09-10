d83d40bd1a1933144916c174e20a7b0dba90384d
Roman Stoliarov <rstoliarov@corelogic.com>
2018-09-10 19:43:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/whizy/myapp/PaintActivity.java
@@ -0,0 +1,57 @@
+package com.example.whizy.myapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class PaintActivity extends AppCompatActivity {
+
+    private PaintView paintView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+
+        ImageButton revertButton = (ImageButton) findViewById(R.id.revertButton);
+        revertButton.setOnClickListener((View v) -> paintView.revert());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_paint_setting, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.normal:
+                paintView.normal();
+                return true;
+                /*
+            case R.id.emboss:
+                paintView.emboss();
+                return true;*/
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
